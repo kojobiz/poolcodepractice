4fb3eb86a74e9b19f756b45ab8a95bd523228adc
lumijoe <spacedesign63@gmail.com>
2024-08-03 09:33:23 +0900

    test

--- tmp/page/career-graduation.php
+++ tmp/page/career-graduation.php
@@ -15,15 +15,15 @@
   <div class="content">
 
     <section>
-      <h1 class="c-secttl is-red"><?php the_title();?></h1>
+      <h1 class="c-secttl is-red"><?php the_title(); ?></h1>
       <div class="c-flex-col2 mt50">
         <div>
           <h2 class="c-secttl-box">進路結果</h2>
-          <img class="mt30" src="<?php echo $imagePath ?>career/graduation/result01.jpg" alt="大学進学率94.9%" width="555" height="352">
+          <img class="mt30" src="<?php echo $imagePath ?>career/graduation/img01_shinrojokyo.jpg" alt="大学進学率91.9%、国公立を含む難関有名大学に72.3%が進学" width="555" height="352">
         </div>
         <div>
           <h2 class="c-secttl-box">4年制大学進学者内訳</h2>
-          <img class="mt10" src="<?php echo $imagePath ?>career/graduation/result02.jpg" alt="国公立を含む難関大学に71.6%が進学" width="518" height="469">
+          <img class="mt10" src="<?php echo $imagePath ?>career/graduation/img02_daigaku.jpg" alt="国公立8名、関関同立24名、産近甲龍56名、主要女子大71名、首都圏18名、医歯薬看護系74名、食物栄養系12名、芸術系12名" width="518" height="469">
         </div>
       </div><!-- /.c-flex-col2 -->
     </section>
